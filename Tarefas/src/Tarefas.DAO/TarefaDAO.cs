23b1d261ed233b4019c29bc45feff4ba3f691424
Alan Avila <afsavila@gmail.com>
2023-01-25 16:47:04 -0300

    Arquivo de DB em inicio

---
+++ Tarefas/src/Tarefas.DAO/TarefaDAO.cs
@@ -0,0 +1,35 @@
+using Dapper;
+using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using Tarefas.DTO;
+
+
+namespace Tarefas.DAO{
+    public class TarefaDAO
+    {
+        public string DataSourceFile => Evironment.CurrentDirectory+"TarefasDB.sqlite";
+        public SQLiteConnection Connection = new SQLiteConnection ("Datasourse="+DataSourceFile);
+    }
+
+    public TarefaDAO
+    {
+        if (!file.Exists(DatasourceFile))
+        {
+            CreateDatabase();
+        }
+    }
+
+    public void CreateDatabase()
+    {
+        using (var con = Connection());
+        con.Open();
+        con.Execute(@"create table Tarefas 
+                    id intereger primary key autoincrement, 
+                    titulo varchar(100), 
+                    descricao varchar(100)")
+
+    }
+
+}
