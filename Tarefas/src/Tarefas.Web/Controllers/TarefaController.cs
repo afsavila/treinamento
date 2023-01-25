9e6e2e92fd7ba5f63b99391bdc720a74d387c27c
Alan Avila <afsavila@gmail.com>
2023-01-25 15:19:18 -0300

    Solução para id inicio de Details

--- Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
@@ -1,11 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
 public class TarefasController : Controller
 {
+    public List<TarefaViewModel> listaDeTarefas{get;set;}
+    public TarefasController ()
+    {        
+        listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel (){ Id = 1, Titulo = "Escovar os dentes", Descricao = "Usar o banheiro"},
+            new TarefaViewModel (){ Id = 2, Titulo = "Tomar cafe", Descricao = "Cafe com pao"},
+            new TarefaViewModel (){ Id = 3, Titulo = "Lavar a roupa", Descricao = "Pegar no cesto e por na maquina"}
+        };
+        
+    }
+    
     public IActionResult Create ()
     {
         return View();
+    }
+
+    public IActionResult Index()
+    {
+        return View(listaDeTarefas);
+    }
+
+    public IActionResult Details (int id)
+    {
+       // var tarefa = listaDeTarefas.Find(tarefa=>Id=id),
+        var tarefa = listaDeTarefas.Find(tarefa=>tarefa.Id==id);
+        return View(tarefa);
     }
 };
